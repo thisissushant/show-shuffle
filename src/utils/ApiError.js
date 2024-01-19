4db5789cfd65f4a1dfb8d8848269aa6083c2cf82
Sushant Singh <sushantbibhu@gmail.com>
2024-01-19 12:44:42 +0530

    add files in utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Somthing went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.status = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+export { ApiError };
