cd98a048495f6d45526ed9378b1d88bb03fdcf1d
Sushant Singh <sushantbibhu@gmail.com>
2024-01-21 14:47:20 +0530

    Add user registration functionality

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,8 +1,59 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   console.log(email);
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are reqired");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "user alredy register");
+  }
+
+  const avtarLocalPath = req.files?.avatar[0]?.path;
+  const coverLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avtarLocalPath) {
+    throw new ApiError(400, "avatar is reqired");
+  }
+  const avatar = await uploadOnCloudinary(avtarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "avatar is reqired");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowercase(),
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "user registration failed");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registerd Successfully"));
 
   //user details from frontend
   //validation - chech whether for empty
