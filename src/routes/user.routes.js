f609dc11171557b4807c605447ac29246771d4ad
Sushant Singh <sushantbibhu@gmail.com>
2024-01-21 08:36:06 +0530

    Update user registration route and controller.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers";
+import { registerUser } from "../controllers/user.controllers.js";
 
 const router = Router();
 router.route("/register").post(registerUser);
