fd8c68befd2ab69eee32521ea26b8d0efc96c4f8
Sushant Singh <sushantbibhu@gmail.com>
2024-01-21 08:38:38 +0530

    Fix typo in console.log statement.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port:${process.env.PRT}`);
+      console.log(`Server is running at port:${process.env.PORT}`);
     });
   })
   .catch((err) => {
