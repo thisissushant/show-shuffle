48dad2775232e92eabdeb90aff2a3b0a951fb54e
Sushant Singh <sushantbibhu@gmail.com>
2024-01-24 11:40:10 +0530

    Update user controller: change findById to findByIdAndUpdate

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -208,7 +208,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
   const { oldPassword, newPassword } = req.body;
-  const user = await User.findById(req.user?.id);
+  const user = await User.findById(req.user?._id);
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
 
   if (!isPasswordCorrect) {
@@ -226,6 +226,26 @@ const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(200, req.user, "Current user fetched successfully");
+});
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullname, email } = req.body;
+
+  if (fullname || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+  User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullname: fullname,
+        email: email,
+      },
+    },
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Profile updated Successfully"));
 });
 
 export {
