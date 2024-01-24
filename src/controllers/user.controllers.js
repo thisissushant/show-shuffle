086342ed963a9b12294fe5b7aee1d167a1fe6f7b
Sushant Singh <sushantbibhu@gmail.com>
2024-01-24 11:12:04 +0530

    Add API route to fetch current user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -222,10 +222,17 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(200, req.user, "Current user fetched successfully");
+});
+
 export {
   registerUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
 };
