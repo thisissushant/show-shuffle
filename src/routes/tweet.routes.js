89ab14db18f82b524b1d93cbe640162aac182030
Sushant Singh <sushantbibhu@gmail.com>
2024-01-31 16:36:40 +0530

    add tweet and video routes

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  createTweet,
+  updateTweet,
+  deleteTweet,
+  getUserTweets,
+} from "../controllers/tweet.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT); // apply middleware to all routes
+
+router.route("/").post(createTweet);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router.route("/:userId").get(getUserTweets);
+
+export default router;
