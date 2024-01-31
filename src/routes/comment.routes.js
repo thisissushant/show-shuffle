fc3cef4d20f8258e72f0cc690f16fa23b4088acb
Sushant Singh <sushantbibhu@gmail.com>
2024-01-31 16:42:38 +0530

    add comment router

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import {
+  addCommentToVideo,
+  addCommentToTweet,
+  updateCommentToVideo,
+  updateCommentToTweet,
+  deleteCommentToVideo,
+  deleteCommentToTweet,
+  getVideoComments,
+  getTweetComments,
+} from "../controllers/comment.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT); // apply in all
+
+router.route("/video/:videoId").get(getVideoComments).post(addCommentToVideo);
+router.route("/tweet/:tweetId").get(getTweetComments).post(addCommentToTweet);
+
+router
+  .route("/video/c/:commentId")
+  .delete(deleteCommentToVideo)
+  .patch(updateCommentToVideo);
+router
+  .route("/tweet/c/:commentId")
+  .delete(deleteCommentToTweet)
+  .patch(updateCommentToTweet);
+
+export default router;
